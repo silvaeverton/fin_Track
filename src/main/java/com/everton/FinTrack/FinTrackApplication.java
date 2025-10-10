33b39f7946037f294de2c55077e464715a59e6d6
silvaeverton <168092881+silvaeverton@users.noreply.github.com>
2025-10-10 11:03:24 -0300

    Update FinTrackApplication.java

--- src/main/java/com/everton/FinTrack/FinTrackApplication.java
+++ src/main/java/com/everton/FinTrack/FinTrackApplication.java
@@ -2,6 +2,7 @@ package com.everton.FinTrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
